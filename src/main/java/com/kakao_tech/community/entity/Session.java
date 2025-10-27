d1254a474d26513b40c22523cf6e74acbb51b665
Lee Jihun <ljh09060@gmail.com>
2025-10-27 21:28:26 +0900

    refactor: sessionKey -> sessionId 명칭 변경

--- src/main/java/com/kakao_tech/community/entity/Session.java
+++ src/main/java/com/kakao_tech/community/entity/Session.java
@@ -6,9 +6,6 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-// TODO : 대충 만든거라 다시 세심하게 검토해볼 필요가 있음.
-// + sessionKey는 유니크로 변경해야함. 키값이 겹치면 어떤 사용자로 로그인한건지 나중에 판단 불가능임.
-
 @Entity
 @Getter
 @Setter
@@ -19,8 +16,8 @@ public class Session {
     @Column(columnDefinition = "BIGINT")
     private Long id;
 
-    @Column(unique = false, nullable = false, length = 10)
-    private String sessionKey;
+    @Column(unique = false, nullable = false, length = 64)
+    private String sessionId;
 
     @Column(unique = false, nullable = false, columnDefinition = "DATETIME")
     private LocalDateTime createdAt;
@@ -34,8 +31,8 @@ public class Session {
 
     protected Session() {}
 
-    public Session(String sessionKey, User user) {
-        this.sessionKey = sessionKey;
+    public Session(String sessionId, User user) {
+        this.sessionId = sessionId;
         this.createdAt = LocalDateTime.now();
         this.expiredAt = LocalDateTime.now().plusMinutes(30);
 
