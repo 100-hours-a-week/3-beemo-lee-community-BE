2fd62e7ae41ed6eaf722122297ba48242b14d487
Leejihun <ljh09060@gmail.com>
2025-10-28 20:02:21 +0900

    delete: 유저 서비스에서 generateAndSaveSessionId 메서드를 통해 날짜 초기화 되므로 삭제

--- src/main/java/com/kakao_tech/community/entity/Session.java
+++ src/main/java/com/kakao_tech/community/entity/Session.java
@@ -2,13 +2,14 @@ package com.kakao_tech.community.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
+@RequiredArgsConstructor
 @Table(name = "sessions")
 public class Session {
     @Id
@@ -26,18 +27,9 @@ public class Session {
     private LocalDateTime expiredAt;
 
     // 세션 무효화 하면 세션을 바로 삭제하는 방향으로 진행
-
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", columnDefinition = "INT")
     private User user;
 
-    protected Session() {}
-
-    public Session(String sid, User user) {
-        this.sid = sid;
-        this.createdAt = LocalDateTime.now();
-        this.expiredAt = LocalDateTime.now().plusMinutes(30);
-
-        this.user = user;
-    }
 }
