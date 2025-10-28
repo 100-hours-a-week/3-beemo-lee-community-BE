dfa2d8ca6d4b3b169200d1b52d6f99194ebf6bbd
Leejihun <ljh09060@gmail.com>
2025-10-28 20:29:42 +0900

    delete: 게시글 생성 기능 주석처리

--- src/main/java/com/kakao_tech/community/controller/PostController.java
+++ src/main/java/com/kakao_tech/community/controller/PostController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kakao_tech.community.dto.PostDTO;
 import com.kakao_tech.community.dto.SessionDTO;
 import com.kakao_tech.community.entity.User;
-import com.kakao_tech.community.service.AuthService;
 import com.kakao_tech.community.service.PostService;
 
 import lombok.AllArgsConstructor;
@@ -28,9 +27,6 @@ public class PostController {
     @Autowired
     public final PostService postService;
 
-    @Autowired
-    public final AuthService authService;
-
     @GetMapping
     // GET - '/posts?limit=10'
     // GET - '/posts?limit=10&offset=10'
@@ -40,19 +36,19 @@ public class PostController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping
-    public ResponseEntity<?> createPost(@RequestBody PostDTO.CreateRequest request,
-            @RequestHeader(value = "Cookie", required = false) String cookie) {
-        // 세션 인증 후, user 객체 넘기기
-        SessionDTO.RequestCookie session = new SessionDTO.RequestCookie(cookie);
-        User user = authService.getCurrentUser(session);
+    // @PostMapping
+    // public ResponseEntity<?> createPost(@RequestBody PostDTO.CreateRequest request,
+    //         @RequestHeader(value = "Cookie", required = false) String cookie) {
+    //     // 세션 인증 후, user 객체 넘기기
+    //     SessionDTO.RequestCookie session = new SessionDTO.RequestCookie(cookie);
+    //     User user = authService.getCurrentUser(session);
 
-        // 게시글 작성 서비스 호출
-        Long postId = postService.createPost(request.getTitle(), request.getBody(), request.getImageUrl(), user);
+    //     // 게시글 작성 서비스 호출
+    //     Long postId = postService.createPost(request.getTitle(), request.getBody(), request.getImageUrl(), user);
 
-        return ResponseEntity
-            .created(URI.create("/api/posts/" + postId))
-            .build();
-    }
+    //     return ResponseEntity
+    //         .created(URI.create("/api/posts/" + postId))
+    //         .build();
+    // }
 
 }
