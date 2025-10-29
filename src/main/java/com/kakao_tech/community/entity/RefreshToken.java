aca7a54481551080150c32458959273cb91a5716
Leejihun <ljh09060@gmail.com>
2025-10-29 16:06:24 +0900

    feat: 리프래시 토큰 저장하는 엔티티 추가

---
+++ src/main/java/com/kakao_tech/community/entity/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.kakao_tech.community.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter @Setter
+@RequiredArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String token;
+
+    private Instant expiresAt;
+
+    private boolean revoked;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "INT")
+    private User user;
+}
