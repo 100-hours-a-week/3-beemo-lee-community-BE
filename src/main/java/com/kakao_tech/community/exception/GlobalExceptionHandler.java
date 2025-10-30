75e5d00a5eafb283fab4e3393e6b96a169d19c1c
Lee Jihun <ljh09060@gmail.com>
2025-10-30 19:53:35 +0900

    refactor: error response 자료형 수정

--- src/main/java/com/kakao_tech/community/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakao_tech/community/exception/GlobalExceptionHandler.java
@@ -4,13 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.kakao_tech.community.dto.ErrorDTO;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RestApiException.class)
-    public ResponseEntity<ErrorResponse> restApiExceptionHandler(RestApiException e) {
+    public ResponseEntity<ErrorDTO.Response> restApiExceptionHandler(RestApiException e) {
         CustomErrorCode errorCode = e.getCustomErrorCode();
-        ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+        ErrorDTO.Response errorResponse = new ErrorDTO.Response(errorCode.getCode(), errorCode.getMessage());
         return ResponseEntity.status(errorCode.getHttpStatus()).body(errorResponse);
     }
 
